9b670f794287660213f3f9cedd156d20b34a68b6
FedorShuvalov <shuvalov.fedor18@outlook.com>
2020-11-08 18:33:05 +0500

    Добавьте файлы проекта.

---
+++ OPZcalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OPZcalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
